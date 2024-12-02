f7a468e8cda964cb54460a3a50eff608f2296be5
Zezhen Wang <zezhenw@umich.edu>
2024-12-02 17:37:31 -0500

    add ingredient and technique

--- Cocktail.py
+++ Cocktail.py
@@ -1,16 +1,21 @@
+import re 
+
 class cocktail_ingredients:
-    def __init__(self, ingredient, amount) -> None:
+    def __init__(self, amount, ingredient) -> None:
         self.ingredient = ingredient
-        self.amount = amount
+        self.amount = amount.split()[0]
+        self.unit = amount.split()[1]
+        
     def info(self):
-        return self.ingredient + "   " + self.amount
+        return self.ingredient + ": " + self.amount + ' ' + self.unit
 
 class Cocktail:
     def __init__(self, title, glass, garnish, recipe, ingredients) -> None:
         self.name = title
         self.glass = glass
-        self.garnish = garnish 
+        self.garnish = garnish
         self.recipe = recipe
+        self.technique = list(set(re.findall(r'\b[A-Z]{2,}\b', recipe)))
         self.ingredients = [] # [(A, amount), (B, amount)]
         for item in ingredients:
             self.ingredients.append(cocktail_ingredients(item[0], item[1]))
@@ -21,7 +26,9 @@ class Cocktail:
         for item in self.ingredients:
             print(item.info())
         print("Glass: " + self.glass)
-        print("Garnish: " + self.garnish)
+        print("Garnish: " + ("None" if type(self.garnish) is not str else self.garnish))
         print("Recipe: " + self.recipe)
+        print("Technique: " + str(self.technique))
+        print()
 
     
